993a61e723f2712ee2b53dcf3496d170095d3f25
Daniel Abayomi <dan234bayomi@gmail.com>
2023-09-22 04:19:06 -0700

    Finished first task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { 'create-a-file':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,5 @@
+# Description
+This is the Server Configuration Management section for the ALX SE program.
+
+# Notes
+* None for now
