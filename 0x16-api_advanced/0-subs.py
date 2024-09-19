8a6b0beacdaea45458a682104eab87c5db21c58c
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-19 11:04:26 -0700

    Figuring out task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import requests
-"""Number of subscribers check"""
+"""
+Number of subscribers check
+"""
 
 
 def number_of_subscribers(subreddit):
@@ -13,19 +15,16 @@ def number_of_subscribers(subreddit):
         subscriber count if subreddit exists,
         0, otherwise
     """
-    api_url = f"https://www.reddit.com//r/{subreddit}/top.json"
-    params = {
-        'limit': 10,
-        't': 'month'
-    }
+    api_url = f"https://www.reddit.com//r/{subreddit}/about.json"
 
-    response = requests.get(api_url, params,
+    response = requests.get(api_url,
                             headers={
-                                'User-Agent': 'BadBunnyN47WhoLikedNikky'
+                                "User-Agent": 'BadBunnyN47WhoLikedNikky'
                                 }, allow_redirects=False)
     if response.status_code != 200:
         return 0
 
     response_data = response.json()
-    sub = response_data['data']['children'][0]['data']['subreddit_subscribers']
+    sub = response_data['data']['subscribers']
     return sub
+
