e9b212b07ad4f2cff6f5720f4d852f6c1ab83e4a
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-19 11:49:03 -0700

    Confirming if API works on checker

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+"""Number of subscribers check"""
 import requests
-"""
-Number of subscribers check
-"""
 
 
 def number_of_subscribers(subreddit):
@@ -15,16 +13,12 @@ def number_of_subscribers(subreddit):
         subscriber count if subreddit exists,
         0, otherwise
     """
+    headers = {'User-Agent': 'BadBunnyN47WhoLikedNikky'}
     api_url = f"https://www.reddit.com//r/{subreddit}/about.json"
 
-    response = requests.get(api_url,
-                            headers={
-                                "User-Agent": 'BadBunnyN47WhoLikedNikky'
-                                }, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
-
-    response_data = response.json()
-    sub = response_data['data']['subscribers']
-    return sub
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    res = response.json()
+    if response.status_code == 200:
+        return (res['data']['subscribers'])
+    return (0)
 
