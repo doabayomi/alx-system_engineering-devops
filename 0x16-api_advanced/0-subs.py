eca5b2a216bfe8343aa688301b41665124f633f9
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-19 11:56:43 -0700

    Final task 1 attempt

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,21 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets the number of subscribers in a subreddit
-
-    Args:
-        subreddit: Subreddit name
-
-    Returns:
-        subscriber count if subreddit exists,
-        0, otherwise
-    """
     headers = {'User-Agent': 'BadBunnyN47WhoLikedNikky'}
     api_url = f"https://www.reddit.com//r/{subreddit}/about.json"
 
     response = requests.get(api_url, headers=headers, allow_redirects=False)
-    res = response.json()
+
     if response.status_code == 200:
-        return (res['data']['subscribers'])
-    return (0)
+        res_data = response.json()
+        return res_data['data']['subscribers']
+    else:
+        return 0
 
