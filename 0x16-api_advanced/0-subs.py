70b6a0ab795f7a17f768433be6330e646bb40297
Daniel Abayomi <dan234bayomi@gmail.com>
2024-09-19 12:32:03 -0700

    Confirming if oauth works

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,21 @@
 """Number of subscribers check"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'BadBunnyN47WhoLikedNikky'}
-    api_url = f"https://www.reddit.com//r/{subreddit}/about.json"
+    with open('reddit_token.txt', 'r') as file:
+        token = file.read().strip()
+
+    headers = {
+        'Authorization': f"bearer {token}",
+        'User-Agent': 'alx:api-advanced-project (by /u/Bad_Bunny_D)'
+        }
+    api_url = f"https://oauth.reddit.com//r/{subreddit}/about.json"
 
     response = requests.get(api_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        res_data = response.json()
-        return res_data['data']['subscribers']
+        data = response.json()
+        return data['data']['subscribers']
     else:
         return 0
 
